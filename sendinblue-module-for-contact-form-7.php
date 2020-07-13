8b8c7e0d3c2a832382dd4da789f067c3f741ef57
Takayuki Miyoshi <takayukister@gmail.com>
2020-07-13 22:14:43 +0900

    Basic structure as a WordPress plugin

---
+++ sendinblue-module-for-contact-form-7.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Sendinblue module for Contact Form 7
+Description: Just another Sendinblue module for Contact Form 7.
+Author: Takayuki Miyoshi
+Text Domain: sendinblue-module-for-contact-form-7
+Domain Path: /languages/
+Version: 0.7-dev
+*/
+
+define( 'CF7SENDINBLUE_PLUGIN', __FILE__ );
+
+define( 'CF7SENDINBLUE_PLUGIN_BASENAME',
+  plugin_basename( CF7SENDINBLUE_PLUGIN )
+);
+
+define( 'CF7SENDINBLUE_PLUGIN_NAME',
+  trim( dirname( CF7SENDINBLUE_PLUGIN_BASENAME ), '/' )
+);
+
+define( 'CF7SENDINBLUE_PLUGIN_DIR',
+  untrailingslashit( dirname( CF7SENDINBLUE_PLUGIN ) )
+);
+
+define( 'CF7SENDINBLUE_PLUGIN_MODULES_DIR',
+  CF7SENDINBLUE_PLUGIN_DIR . '/modules'
+);
+
+add_action( 'plugins_loaded', function() {
+  $dir = CF7SENDINBLUE_PLUGIN_MODULES_DIR;
+
+  if ( empty( $dir ) or ! is_dir( $dir ) ) {
+    return false;
+  }
+
+  $file = path_join( $dir, 'sendinblue/sendinblue.php' );
+
+  if ( file_exists( $file ) ) {
+    include_once $file;
+  }
+}, 20, 0 );
